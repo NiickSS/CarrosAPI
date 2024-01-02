f5ce1fa01e175100ba484e43939462cfa92e3a55
NiickSS <nicolas.juner.marcari@gmail.com>
2024-01-02 17:23:52 -0300

    Arrumando conexao

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose, {mongo} from "mongoose";
 
 async function conectaBD(){
-    mongoose.connect("mongodb+srv://root:TestandoAPIDeCarros@cluster0.kapodod.mongodb.net/?retryWrites=true&w=majority");
+    mongoose.connect("Inserir BD");
     return mongoose.connection;
 };
 
