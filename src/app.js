3ae65d9d9227a80ac3848754c9d09422eb47d041
NiickSS <nicolas.juner.marcari@gmail.com>
2024-01-02 16:37:50 -0300

    validando conexao com BD

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
 import express from "express";
+import conectaBD from "./config/dbConnect.js";
+import routes from "./routes/index.js";
+
+const conexao = await conectaBD();
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexao", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conexao com BD feita!");
+});
 
 const app = express();
+routes(app);
+
+export default app;
